1791b4ee67019910c426b6d77f8a132770be549f
AmericanWonton <jbkeller0303@gmail.com>
2020-03-21 15:53:12 -0500

    Added some gohtml files and will experimenting with those first.

--- project/main.go
+++ project/main.go
@@ -1,10 +1,37 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 )
+
+/* TEMPLATE DEFINITION BEGINNING */
+var template1 *template.Template
+
+func init() {
+	template1 = template.Must(template.ParseGlob("gotempls/*"))
+}
+
+/* EXECUTING TEMPLATES BEGINNING */
+// Handle Errors
+func HandleError(w http.ResponseWriter, err error) {
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatalln(err)
+	}
+}
+
+//index.gohtml
+func indexExecute(w http.ResponseWriter, _ *http.Request) {
+	err1 := template1.ExecuteTemplate(w, "index.gohtml", nil)
+	HandleError(w, err1)
+}
+
+/* EXECUTING TEMPLATES END */
+
+/* TEMPLATE DEFINITION ENDING */
 
 func main() {
 
@@ -24,4 +51,7 @@ func main() {
 	}
 
 	/* ------------- SERVING FILES ENDING ---------------------- */
+
+	/* ----------------- TEMPLATE EXECUTION BEGINNING -------------- */
+	http.HandleFunc("/", indexExecute)
 }
