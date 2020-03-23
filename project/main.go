7ce430603905a942c26f96e7817f9e2e82fa9868
AmericanWonton <jbkeller0303@gmail.com>
2020-03-22 19:20:30 -0500

    Added some files and messed with Facebook APIs

--- project/main.go
+++ project/main.go
@@ -29,6 +29,13 @@ func indexExecute(w http.ResponseWriter, _ *http.Request) {
 	HandleError(w, err1)
 }
 
+//bookdjmark.gohtml
+func bookdjExecute(w http.ResponseWriter, _ *http.Request) {
+	/*Execute template, handle error */
+	err1 := template1.ExecuteTemplate(w, "bookdjmark.gohtml", nil)
+	HandleError(w, err1)
+}
+
 /* EXECUTING TEMPLATES END */
 
 /* TEMPLATE DEFINITION ENDING */
@@ -36,8 +43,11 @@ func indexExecute(w http.ResponseWriter, _ *http.Request) {
 func main() {
 	/* ----------------- TEMPLATE EXECUTION BEGINNING -------------- */
 	http.HandleFunc("/", indexExecute)
+	http.HandleFunc("/bookdjmark.gohtml", bookdjExecute)
+	/* ---------------- TEMPLATE EXECUTION ENDING ---------------- */
 	/* Handle all files in the static path */
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	//http.Handle("/static/jpg", http.StripPrefix("/static/jpg", http.FileServer(http.Dir("static/jpg"))))
 	/* Listen and serve */
 	http.ListenAndServe(":8080", nil)
 }
